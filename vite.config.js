4da5238e6bb191d57f3e13989d5cb19cabadc32b
Evelina Norlin <evelina.norlin@hotmail.com>
2022-12-29 15:46:52 +0100

    ändrat base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ då ska "base" här nedan vara "/fed22d-js-grundkurs-jenni-wumpus/"
  */
 
 export default defineConfig({
-  base: '/fed22d-js-grundkurs-2-webbplats-evelinanorlin/', // TODO - ändra till ditt repo-namn
+  base: '/norlin_movement/', // TODO - ändra till ditt repo-namn
   define: {
     'process.env': {},
   },
